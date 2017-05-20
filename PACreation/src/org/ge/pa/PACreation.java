582aebc4dafcf96ce4c868fef3b31422de889c72
sureshkrishnaGE <sureshkrishna0517@gmail.com>
2017-05-20 14:23:18 +0800

    This is PA Creation Project

---
+++ PACreation/src/org/ge/pa/PACreation.java
@@ -0,0 +1,9 @@
+package org.ge.pa;
+
+public class PACreation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Pa Creation class");
+	}
+}
